564813d552eded6e290d101fa6e78e72f37c97a2
jakobistjakob <j.j.martin@web.de>
2025-01-06 11:37:55 +0100

    Admins können noch alle Chats herunterladen aber NICHT mehr lesen. In der Sidebar ist Dalle und Prompt-Tipps

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -499,12 +499,46 @@
 						/>
 					</div>
 					<div class=" self-center font-medium text-sm text-gray-850 dark:text-white font-primary">
-						TestTest
+						{$i18n.t('New Chat')}
 					</div>
 				</div>
 
 				<div>
 					<PencilSquare className=" size-5" strokeWidth="2" />
+				</div>
+			</a>
+		</div>
+
+		<div class="px-2 flex justify-center mt-0.5">
+			<a
+			class="flex-grow flex space-x-3 rounded-xl px-3.5 py-2 hover:bg-gray-200 dark:hover:bg-gray-900 transition"
+			href="https://copilot.microsoft.com/"
+			target="_blank"
+			title="Bildgenerierung mit Dalle auf Copilot">
+				<div class="self-center">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h18v18H3V3zm3 16l3-3 3 3 3-3 3 3"></path>
+					</svg>
+				</div>
+				<div class="flex self-center">
+					<div class="self-center font-medium text-sm">Dalle</div>
+				</div>
+			</a>
+		</div>
+
+		<div class="px-2 flex justify-center mt-0.5">
+			<a
+			class="flex-grow flex space-x-3 rounded-xl px-3.5 py-2 hover:bg-gray-200 dark:hover:bg-gray-900 transition"
+			href="https://fimrc.sharepoint.com/sites/IT-Services/Apps/SitePages/Tipps-f%C3%BCr-Prompteingaben-bei-Large-L.aspx"
+			target="_blank"
+			title="Tipps zur Prompteingabe">
+				<div class="self-center">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h18v18H3V3zm3 16l3-3 3 3 3-3 3 3"></path>
+					</svg>
+				</div>
+				<div class="flex self-center">
+					<div class="self-center font-medium text-sm">Tipps für Prompts</div>
 				</div>
 			</a>
 		</div>
